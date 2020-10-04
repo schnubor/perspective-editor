5c337814d1458a6d7e6fdda537b9fa96de806b86
Christian Korndoerfer <christian@perspective.co>
2020-10-04 10:44:39 +0200

    insert at index

--- src/Sidebar/Sidebar.svelte
+++ src/Sidebar/Sidebar.svelte
@@ -2,8 +2,11 @@
     import Block from './Block.svelte';
     import { activeBlock } from '../stores/artboard';
     import blockMap from '../Blocks/map';
+    import { dndzone, TRIGGERS } from 'svelte-dnd-action';
 
-    const blockList = Object.keys(blockMap).map((block) => {
+    let shouldIgnoreDndEvents = false;
+
+    let blockList = Object.keys(blockMap).map((block) => {
         return {
             id: block.toLowerCase(),
             name: block,
@@ -11,10 +14,39 @@
     });
 
     const handleEditClose = () => {
-        activeBlock.update((currentActiveBlock) => {
+        activeBlock.update(() => {
             return {};
         });
     };
+
+    const handleDndConsider = (e) => {
+        console.warn(`got consider ${JSON.stringify(e.detail, null, 2)}`);
+        const {trigger, id} = e.detail.info;
+        if (trigger === TRIGGERS.DRAG_STARTED) {
+            console.warn(`copying ${id}`);
+            const idx = blockList.findIndex(item => item.id === id);
+            const newId = `${id}_copy_${Math.round(Math.random()*100000)}`;
+            e.detail.items.splice(idx, 0, {...blockList[idx], id: newId});
+            blockList = e.detail.items;
+            shouldIgnoreDndEvents = true;
+        }
+        else if (!shouldIgnoreDndEvents) {
+            blockList = e.detail.items;
+        }
+        else {
+            blockList = [...blockList];
+        }
+    }
+    const handleDndFinalize = (e) => {
+        console.warn(`got finalize ${JSON.stringify(e.detail, null, 2)}`);
+        if (!shouldIgnoreDndEvents) {
+            blockList = e.detail.items;
+        }
+        else {
+            blockList = [...blockList];
+            shouldIgnoreDndEvents = false;
+        }
+    }
 </script>
 
 <aside>
@@ -23,16 +55,17 @@
         <div class="close" on:click={handleEditClose}>Close</div>
     {:else}
         <div class="blocks">
-        {#each blockList as block, index (block.id)}
-            <Block block={block} />
-        {/each}
+            <section use:dndzone={{items: blockList, dropFromOthersDisabled: true}} on:consider={handleDndConsider} on:finalize={handleDndFinalize}>
+                {#each blockList as block(block.id)}
+                    <Block block={block} />
+                {/each}
+            </section>
         </div>
     {/if}
 </aside>
 
 <style>
     aside {
-        background: #dddddd;
         width: 360px;
         height: 100%;
         padding: 32px;
